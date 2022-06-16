33c67390cd960c040422d66de1a6e5261a9d4aad
前田 <sloth19920731@gmail.com>
2022-06-16 23:57:12 +0900

    ルーティングとテンプレート部分(cssまだ)

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
 import {RegisterComponent} from '../auth/components/register/register.component'
 
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
