9fc1feedc02f518cc701e5cbaa56587172d12650
前田 <sloth19920731@gmail.com>
2022-06-21 14:37:42 +0900

    effect・action.reducer更新

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -2,6 +2,7 @@ import {createAction, props} from '@ngrx/store'
 import {RegisterRequestinterface} from 'src/app/shared/types/registerRequest.interface'
 import {ActionTypes} from 'src/app/auth/store/actionTypes'
 import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
+import {BackendErrorsInterface} from 'src/app/shared/types/backendErrors.interface'
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
@@ -13,4 +14,7 @@ export const registerSuccessAction = createAction(
   props<{currentUser: CurrentUserInterface}>()
 )
 
-export const registerFailureAction = createAction(ActionTypes.REGISTER_FAILURE)
+export const registerFailureAction = createAction(
+  ActionTypes.REGISTER_FAILURE,
+  props<{errors: BackendErrorsInterface}>()
+)
--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -10,6 +10,7 @@ import {
 import {AuthService} from '../../services/auth.service'
 import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
 import {of} from 'rxjs'
+import {HttpErrorResponse} from '@angular/common/http'
 
 @Injectable()
 export class RegisterEffect {
@@ -22,8 +23,10 @@ export class RegisterEffect {
             return registerSuccessAction({currentUser})
           }),
 
-          catchError(() => {
-            return of(registerFailureAction())
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              registerFailureAction({errors: errorResponse.error.errors})
+            )
           })
         )
       })
