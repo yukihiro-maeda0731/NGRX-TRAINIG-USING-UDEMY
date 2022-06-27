37077e1acd8d6f967a73aca8e698d076530513be
前田 <sloth19920731@gmail.com>
2022-06-27 23:50:38 +0900

    8の終わりまで　app.module.tsから子moduleのauth.moduleをインポートして、auth.moduleで宣言しているregistercomponentを表示

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/components/register/register.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {AuthModule} from './auth/auth.module'
+import {RegisterComponent} from './auth/components/register/register.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+import {RegisterComponent} from './components/register/register.component'
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [RegisterComponent],
 })
 export class AuthModule {}
