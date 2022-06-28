a7dd82e77c5ed1b60ebad4e6b27839de23a4b9ea
前田 <sloth19920731@gmail.com>
2022-06-28 20:42:49 +0900

    11の終わりまでngrx/storedev-toolsパッケージ入れて、ブラウザで登録ボタン押すとActionがdispatchされていることを確認

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import {BrowserModule} from '@angular/platform-browser'
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {AuthModule} from './auth/auth.module'
-import {StoreModule} from '@ngrx/store'
+import {StoreModule} from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,7 @@ import {StoreModule} from '@ngrx/store'
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
