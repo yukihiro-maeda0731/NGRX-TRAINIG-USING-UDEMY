d9375aaaf0835211accd9e43c236e741cc7f1e7b
前田 <sloth19920731@gmail.com>
2022-06-28 21:03:48 +0900

    12終わりまで　interfaceを作ってActionのpropsのパラメータをまとめて簡略化

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,8 +1,9 @@
 import {createAction, props} from '@ngrx/store'
+import {RegisterRequestInterface} from '../types/registerRequest.interface'
 import {ActionTypes} from './actionTypes'
 
 // registerActionはregister.component.tsから呼ばれている
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{username: string; password: string; email: string}>()
+  props<{request: RegisterRequestInterface}>()
 )
